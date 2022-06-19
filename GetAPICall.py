2fb6a7a7ccb5c84b08b0309d33695f09b7cc8bb4
kayocallaghan <91154142+kayocallaghan@users.noreply.github.com>
2022-06-19 17:39:38 +0100

    Update GetAPICall.py

--- GetAPICall.py
+++ GetAPICall.py
@@ -1,12 +1,20 @@
 # Make get calls to NVD API to retrieve vulnerability data
 import requests
+import json
 
 # api-endpoint
 URL = "https://services.nvd.nist.gov/rest/json/cves/1.0/"
 
-r = requests.get(url=URL)
-response = r.status_code
-print(response)
+# r = requests.get(url=URL)
+# response = r.status_code
+# print(response)
+
+response = requests.get(URL)
+dic = json.loads(response)
+json_obj = json.dumps(dic, indent=2)
+
+with open("out/output.txt", "w") as f:
+   f.write(json_obj)
 
 
 
